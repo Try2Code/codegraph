b3cc37a20e66fd8a8aa1f430d518fefdc379fc54
Ralf Mueller <ralf.mueller@zmaw.de>
2012-05-09 15:30:27 +0200

    add display method

--- test/graph.rb
+++ test/graph.rb
@@ -15,27 +15,34 @@ class TestGraph < Test::Unit::TestCase
   @@test0_f90 = [@@testdir,FORTRAN_SOURCE_0].join(File::SEPARATOR)
   @@test1_f90 = [@@testdir,FORTRAN_SOURCE_1].join(File::SEPARATOR)
 
-  def test_funcgraph
+  def display(graph,filename)
+    graph.save(filename,'png')
+    system("qiv #{filename}.png") if 'thingol' == `hostname`.chomp
+  end
+
+  def testFG
     filelist = [@@test0_f90,@@test1_f90]
     fg = FunctionGraph.new({:filelist => filelist})
     fg.scan
     ofile = 'testfncgraph'
-    fg.save(ofile,'png')
-    system("qiv #{ofile}.png") if 'thingol' == `hostname`.chomp
+    display(fg,ofile)
   end
-  def test_singleFG
+  def testSFG
     filelist = [@@test0_f90,@@test1_f90]
     sg = SingleFunctionGraph.new(:filelist => filelist,:func => 'transfer')
     ofile = 'testsg'
-    sg.save(ofile,'png')
-    system("qiv #{ofile}.png") if 'thingol' == `hostname`.chomp
+    display(sg,ofile)
   end
-  def test_UFG
+  def testUFG
     filelist = [@@test0_f90,@@test1_f90]
     sg = UpperFunctionGraph.new(:filelist => filelist,:func => 'xfer_idx_2',:debug => false)
     ofile = 'testufg'
-    sg.save(ofile,'png')
-    system("qiv #{ofile}.png") if 'thingol' == `hostname`.chomp
+    display(sg,ofile)
+  end
+  def test8FG
+    filelist = [@@test0_f90,@@test1_f90]
+    efg = EightFunctionGraph.new(:filelist => filelist,:func => 'xfer_idx_3',:debug => true)
+    display(efg,'test8fg')
   end
 
   if `hostname`.chomp == 'thingol' then
